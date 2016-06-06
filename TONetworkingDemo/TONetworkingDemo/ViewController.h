63505bc4a753a0fc4ca94804b2fa934ebabdbc9d
Tony <show_3@163.com>
2016-06-06 10:48:35 +0800

    init project

---
+++ TONetworkingDemo/TONetworkingDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TONetworkingDemo
+//
+//  Created by Tony on 16/6/6.
+//  Copyright © 2016年 Tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TONetworkingDemo/TONetworkingDemo/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  TONetworkingDemo
+//
+//  Created by Tony on 16/6/6.
+//  Copyright © 2016年 Tony. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "TOTask.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    TOTask * task = [[TOTask alloc] initWithPath:@"http://www.baidu.com" parames:nil taskOver:^(TOTask *task) {
+        
+    }];
+    [task startAtOnce];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
